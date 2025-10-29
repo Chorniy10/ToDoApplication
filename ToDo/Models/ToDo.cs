ef6fececb5052cfa7842a3a4db73dea219e9ea9a
Pavlo Maksymchuk <maksimcukpasha@gmail.com>
2025-10-29 15:37:29 +0200

    Add project files.

---
+++ ToDo/Models/ToDo.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDo.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Будь ласка введіть опис завдання")]
+        public string Description { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Будь ласка введіть дату")]
+        public DateTime? DueDate { get; set; }
+        [Required(ErrorMessage = "Будь ласка виберіть категорію")]
+        public string CategoryId { get; set; } = string.Empty;
+        [ValidateNever]
+        public Category Category { get; set; } = null!;
+        [Required(ErrorMessage = "Будь ласка виберіть статус")]
+        public string StatusId { get; set; } = string.Empty;
+        [ValidateNever]
+        public Status Status { get; set; } = null!;
+
+        public bool OverDue => StatusId == "open" && DueDate < DateTime.Today;
+
+    }
+}
---
+++ ToDo/Models/ToDoContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDo.Models
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<ToDo> ToDos { get; set; } = null!;
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<Status> Statuses { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = "work", Name = "Робота" },
+                new Category { CategoryId = "personal", Name = "Особисте" },
+                new Category { CategoryId = "shopping", Name = "Покупки" },
+                new Category { CategoryId = "others", Name = "Інше" }
+                );
+
+            modelBuilder.Entity<Status>().HasData(
+                new Status { StatusId = "open", Name = "В процесі" },
+                new Status { StatusId = "completed", Name = "Завершене" }
+                );
+        }
+    }
+}
